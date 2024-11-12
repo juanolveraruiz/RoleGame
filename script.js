e088b10bc029d1a3ef00f39235f2f1cf01db95cf
Juan Olvera <juanolveraruiz@gmail.com>
2024-11-12 01:09:11 -0600

    added some space between the let, const and function. also created a empty function called goStore

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -15,3 +16,7 @@ const goldText = document.querySelector('#goldText');
 const monsterStats = document.querySelector('#monsterStats');
 const monsterName = document.querySelector('#monsterName');
 const monsterHealthText = document.querySelector('#monsterHealth');
+
+function goStore() {
+
+}
